e1bc2c87eca9ebac1554ea25e9f032d1b589b0ed
hiennm99 <hiennm11599@gmail.com>
2023-05-23 18:17:55 +0700

    Update ETL

--- Code/ETL.py
+++ Code/ETL.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from configparser import ConfigParser
 from sqlalchemy import create_engine
-import datetime
-import os
+from datetime import datetime
 
 def connect_database():
     db_host=config['DB']['hostname']
@@ -33,107 +32,133 @@ def extract_data():
     
     # Get the last_update record in datawarehouse
     try:
-        query1=f"""SELECT last_update FROM public.orders"""
+        query1=f"""SELECT MAX(updated_at) as last_update FROM public.d_updatelogs WHERE tbl_name='f_orders'"""
         dwh_data=pd.read_sql_query(query1,con=dwh_engine)
         
     except Exception as e:
         print(e)
     
     if not dwh_data.empty:
-        last_update_dwh=dwh_data['last_update'].max().strftime("%Y-%m-%d")
-        # Get all records which have updatedAt > last_update
+        last_update_dwh=dwh_data['last_update'][0].strftime("%Y-%m-%d %H:%M:%S")
+        # Get all records which have updated_at > last_update
         try:
-            query2=f"""SELECT * FROM cdc.sales_orders_CT WHERE updatedAt >='{last_update_dwh}'"""
+            query2=f"""SELECT * FROM sales.orders WHERE updated_at <='{last_update_dwh}'"""
             df=pd.read_sql_query(query2,con=db_engine)
         except Exception as e:
             print(e)
     else:
-        # Get all records from database
-        try:
-            query2=f"""SELECT * FROM cdc.sales_orders_CT"""
-            df=pd.read_sql_query(query2,con=db_engine)
-        except Exception as e:
-            print(e)      
+        last_update_dwh=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
     print("Extract data from database successfully !!!")
     rawData=f'raw_{runTime}.parquet'
     df.to_parquet(rawData)
 
+def remove_duplicates(dataframe,columns):
+    dataframe=dataframe.sort_values(columns,ascending=False)
+    dataframe=dataframe.drop_duplicates(subset=columns,keep='first')
+    return dataframe
+
+def handle_missing_values(dataframe,fill_values):
+    return dataframe.fillna(fill_values)
+
+def define_record_type(extracted_data):
+    dwh_engine=connect_datawarehouse()
+    
+    # Get all records existing in datawarehouse
+    existing_data = pd.read_sql('SELECT * FROM f_orders', con=dwh_engine)
+    
+    # So sánh dữ liệu mới với dữ liệu đã tồn tại
+    existing_records = pd.merge(existing_data, extracted_data, on='id', how='inner')
+    new_records = extracted_data[~extracted_data['id'].isin(existing_data['id'])]
+    old_records = extracted_data[extracted_data['id'].isin(existing_data['id'])]
+    return new_records,old_records
+
 def transform_data():
     rawFilename=f'raw_{runTime}.parquet'
-    df=pd.read_parquet(rawFilename)
+    raw_data=pd.read_parquet(rawFilename)
     
-    #Drop temporate columns
-    df=df.drop(columns=['__$start_lsn','__$end_lsn','__$seqval','__$update_mask','__$command_id','id','createdAt'])
+    ## Drop column 'id'
+    raw_data=raw_data.drop(columns=['no'])
     
-    #Rename columms with datawarehouse format
-    df=df.rename(columns={
-        '__$operation':'operation',
-        'orderId':'order_id',
-        'storeId':'store_id',
-        'empId':'emp_id',
-        'cusId':'cus_id',
-        'totalPayment':'total_payment',
-        'paymentMethod':'payment_method',
-        'shippingMethod':'shipping_method',
-        'orderStatus':'order_status',
-        'updatedAt':'last_update',
-        }
-    )
-    
-    #Define new records & drop temporate column
-    df_new=df.loc[df['operation']==2]
-    df_new=df_new.drop(columns=['operation'])
-    
-    #Define updated records & drop temporate column
-    df_old=df.loc[df['operation']==4]
-    df_old=df_old.drop(columns=['operation'])
+    ## Drop duplicate values
+    # Remove duplicates
+    transformed_data =remove_duplicates(dataframe=raw_data,columns='id')
+
+    # Handle missing values
+    transformed_data=handle_missing_values(dataframe=transformed_data,fill_values='N/A')
     
     print("Transform data successfully !!!")
-    newData=f'newData_{runTime}.parquet'
-    oldData=f'oldData_{runTime}.parquet'
-    df_new.to_parquet(newData)
-    df_old.to_parquet(oldData)
+    transformed_data.to_parquet(f'transformed_{runTime}.parquet')
 
 def load_data():
     dwh_engine=connect_datawarehouse()
     
-    #Upload new records to datawarehouse
-    newData=f'newData_{runTime}.parquet'
-    df_new=pd.read_parquet(newData)
-    if not df_new.empty:
-        try:
-            df_new.to_sql('orders',con=dwh_engine,if_exists='append',index=False)
-            print("Uploaded new records successfully !!!")
-        except Exception as e:
-            print(e)
-    else:
-        print("No new records !!!")
-    #Update existing records to datawarehouse
-    oldData=f'oldData_{runTime}.parquet'
-    df_old=pd.read_parquet(oldData)
+    # Get transformed data
+    transformed_data=pd.read_parquet(f"transformed_{runTime}.parquet",engine='fastparquet')
+    # Number of transformed records
+    transformed_count=transformed_data.shape[0]
     
-    if not df_old.empty:
-        for row in df_old.iterrows():
+    # Define new records & old records
+    new_records,old_records=define_record_type(transformed_data)
+    #Number of new records
+    new_count=new_records.shape[0]
+    #Number of old records
+    old_count=old_records.shape[0]
+    
+    # Insert new records
+    if new_count>0:
+        new_records['created_at']=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_records['updated_at']=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_records.to_sql('f_orders', con=dwh_engine, if_exists='append', index=False)
+        
+        print(f"Insert {new_count} new records successfully!!!")
+    
+    # Get number of inserted record successfully
+    query=f"""SELECT COUNT(*) FROM f_orders"""
+    result=dwh_engine.execute(query)
+    inserted_count = result.fetchone()[0]
+    inserted_count=inserted_count-old_count
+    
+    # Update old records
+    updated_count=0
+    if old_count>0:
+        for row in old_records.iterrows():
             try:
-                query3=f"""UPDATE orders SET store_id='{row[1]['store_id']}',emp_id='{row[1]['emp_id']}',
-                        cus_id='{row[1]['cus_id']}',subtotal={row[1]['subtotal']},tax={row[1]['tax']},
-                        discount={row[1]['discount']},total_payment={row[1]['total_payment']},payment_method={row[1]['payment_method']},
-                        shipping_method={row[1]['shipping_method']},order_status={row[1]['order_status']},last_update='{row[1]['last_update']}'
-                WHERE order_id='{row[1]['order_id']}'"""
+                id=f"'{row[1]['id']}'"
+                store=f"'{row[1]['store']}'"
+                employee=f"'{row[1]['employee']}'"
+                customer=f"'{row[1]['customer']}'"
+                subtotal=row[1]['subtotal']
+                tax=row[1]['tax']
+                discount=row[1]['discount']
+                total_payment=row[1]['total_payment']
+                payment_method=row[1]['payment_method']
+                shipping_method=row[1]['shipping_method']
+                status=row[1]['status']
+                updated_at=f"""'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'"""
+                query3=f"""UPDATE f_orders SET store={store}, employee={employee}, customer={customer},
+                                            subtotal={subtotal}, tax={tax}, discount={discount},
+                                            total_payment={total_payment}, payment_method={payment_method},
+                                            shipping_method={shipping_method}, status={status}, updated_at={updated_at}
+                        WHERE id={id}"""
                 dwh_engine.execute(query3)
+                updated_count+=1
             except Exception as e:
                 print(e)
         print("Updated old records successfully !!!")
-    else:
-        print("No old records !!!")
+    
+    # Number of loaded records
+    loaded_count=inserted_count+updated_count
+    
+    if loaded_count==transform_data:
+        print("All records loaded successfully!!!")
         
     print("Load data into datawarehouse successfully !!!!")
     
 if __name__ == '__main__':
     config=ConfigParser()
     config.read('config.ini')
-    runTime=datetime.datetime.now().strftime("%Y_%m_%d")
+    runTime=datetime.now().strftime("%Y_%m_%d")
     extract_data()
     transform_data()
     load_data()
