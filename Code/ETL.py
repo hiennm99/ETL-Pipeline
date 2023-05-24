3f9ae4a48097bf9e35f20b18352a5d2c5150125f
hiennm99 <hiennm11599@gmail.com>
2023-05-24 09:41:55 +0700

    2405

--- Code/ETL.py
+++ Code/ETL.py
@@ -26,11 +26,12 @@ def connect_datawarehouse():
     dwh_engine=create_engine(dwh_url)
     return dwh_engine
 
+
 def extract_data():
     db_engine=connect_database()
     dwh_engine=connect_datawarehouse()
     
-    # Get the last_update record in datawarehouse
+    # Get the last_update of the table from Datawarehouse
     try:
         query1=f"""SELECT MAX(updated_at) as last_update FROM public.d_updatelogs WHERE tbl_name='f_orders'"""
         dwh_data=pd.read_sql_query(query1,con=dwh_engine)
@@ -38,8 +39,11 @@ def extract_data():
     except Exception as e:
         print(e)
     
+    
     if not dwh_data.empty:
+        # If the datawarehouse is not empty
         last_update_dwh=dwh_data['last_update'][0].strftime("%Y-%m-%d %H:%M:%S")
+        
         # Get all records which have updated_at > last_update
         try:
             query2=f"""SELECT * FROM sales.orders WHERE updated_at <='{last_update_dwh}'"""
@@ -47,61 +51,70 @@ def extract_data():
         except Exception as e:
             print(e)
     else:
+        # If the datawarehouse is empty
         last_update_dwh=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
-    print("Extract data from database successfully !!!")
-    rawData=f'raw_{runTime}.parquet'
-    df.to_parquet(rawData)
+    print("Extracted data successfully !!!")
+    raw_file=f'raw_{runTime}.parquet'
+    df.to_parquet(raw_file)
 
+# Function to remove duplicate values
 def remove_duplicates(dataframe,columns):
     dataframe=dataframe.sort_values(columns,ascending=False)
     dataframe=dataframe.drop_duplicates(subset=columns,keep='first')
     return dataframe
 
+# Function to handle missing values
 def handle_missing_values(dataframe,fill_values):
     return dataframe.fillna(fill_values)
 
+# Function to define what record type? (New record or Old record)
 def define_record_type(extracted_data):
     dwh_engine=connect_datawarehouse()
     
-    # Get all records existing in datawarehouse
+    # Get all existing records in datawarehouse
     existing_data = pd.read_sql('SELECT * FROM f_orders', con=dwh_engine)
     
-    # So sánh dữ liệu mới với dữ liệu đã tồn tại
+    # Compare existing records with extracted records
     existing_records = pd.merge(existing_data, extracted_data, on='id', how='inner')
     new_records = extracted_data[~extracted_data['id'].isin(existing_data['id'])]
     old_records = extracted_data[extracted_data['id'].isin(existing_data['id'])]
     return new_records,old_records
 
 def transform_data():
-    rawFilename=f'raw_{runTime}.parquet'
-    raw_data=pd.read_parquet(rawFilename)
+    # Create dataframe from raw data
+    raw_file=f'raw_{runTime}.parquet'
+    raw_data=pd.read_parquet(raw_file)
     
     ## Drop column 'id'
     raw_data=raw_data.drop(columns=['no'])
     
-    ## Drop duplicate values
-    # Remove duplicates
+    # Remove duplicate values
     transformed_data =remove_duplicates(dataframe=raw_data,columns='id')
 
     # Handle missing values
     transformed_data=handle_missing_values(dataframe=transformed_data,fill_values='N/A')
     
-    print("Transform data successfully !!!")
-    transformed_data.to_parquet(f'transformed_{runTime}.parquet')
+    print("Transformed data successfully !!!")
+    transform_file=f'transformed_{runTime}.parquet'
+    transformed_data.to_parquet(transform_file)
 
 def load_data():
     dwh_engine=connect_datawarehouse()
     
-    # Get transformed data
-    transformed_data=pd.read_parquet(f"transformed_{runTime}.parquet",engine='fastparquet')
+    # Create dataframe from transformed data
+    transform_file=f'transformed_{runTime}.parquet'
+    transformed_data=pd.read_parquet(transform_file)
+    
     # Number of transformed records
     transformed_count=transformed_data.shape[0]
     
     # Define new records & old records
     new_records,old_records=define_record_type(transformed_data)
+    
     #Number of new records
     new_count=new_records.shape[0]
+    
     #Number of old records
     old_count=old_records.shape[0]
     
@@ -110,14 +123,15 @@ def load_data():
         new_records['created_at']=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new_records['updated_at']=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new_records.to_sql('f_orders', con=dwh_engine, if_exists='append', index=False)
-        
-        print(f"Insert {new_count} new records successfully!!!")
     
     # Get number of inserted record successfully
     query=f"""SELECT COUNT(*) FROM f_orders"""
     result=dwh_engine.execute(query)
     inserted_count = result.fetchone()[0]
     inserted_count=inserted_count-old_count
+
+    if inserted_count==new_count:
+        print(f"Insert {new_count} new records successfully!!!")
     
     # Update old records
     updated_count=0
@@ -145,15 +159,14 @@ def load_data():
                 updated_count+=1
             except Exception as e:
                 print(e)
-        print("Updated old records successfully !!!")
+        print(f"Updated {updated_count} old records successfully !!!")
     
     # Number of loaded records
     loaded_count=inserted_count+updated_count
     
+    # Check loaded records with transformed records
     if loaded_count==transform_data:
         print("All records loaded successfully!!!")
-        
-    print("Load data into datawarehouse successfully !!!!")
     
 if __name__ == '__main__':
     config=ConfigParser()
